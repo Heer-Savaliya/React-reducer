3d3bbba304e343e659382207da94f0e00497080d
Heer Savaliya <heersavaliya65@gmail.com>
2025-05-12 11:22:26 +0530

    design

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 const App = () => {
   return (
     <>
-      <div>
-        <h1>Increment/Decrement counter</h1>
-        <h4>Using React and Redux</h4>
+      <div className='flex items-center justify-center h-[100dvh] flex-col gap-6'>
+        <h1 className='text-gray-500 text-5xl font-semibold font-serif'>Increment/Decrement counter</h1>
+        <h4 className='text-gray-500 text-lg font-semibold italic lowercase'>Using React and Redux</h4>
 
-        <div>
-          <a title='Decrement'><span>-</span></a>
-          <input type="text" name='quantity' value='0' />
-          <a title='Increment'><span>+</span></a>
+        <div className='bg-gray-300 px-7 py-2 text-center'>
+          <a title='Decrement' className='font-bold text-xl'><span>-</span></a>
+          <input className='bg-white font-bold w-[50px] text-center p-3 mx-7 text-gray-500' type="text" name='quantity' value='0' />
+          <a title='Increment' className='font-bold text-xl'><span>+</span></a>
         </div>
       </div>
     </>
