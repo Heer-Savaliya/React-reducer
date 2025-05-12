7a125f4eaf17ba825aad16547d620193a0a1a703
Heer Savaliya <heersavaliya65@gmail.com>
2025-05-12 11:06:20 +0530

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Increment/Decrement counter</h1>
+        <h4>Using React and Redux</h4>
+
+        <div>
+          <a title='Decrement'><span>-</span></a>
+          <input type="text" name='quantity' value='0' />
+          <a title='Increment'><span>+</span></a>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
